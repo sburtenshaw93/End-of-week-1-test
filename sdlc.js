27e114881442cd6d280a71b1955547da0cb0e68d
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-02-17 17:36:00 -0500

    updated and final version of week 1 test

--- sdlc.js
+++ sdlc.js
@@ -18,10 +18,10 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const Analysis = ''
-const Design = ''
-const Implementation = ''
-const Testing = ''
-const Integration = ''
-const Maintain = ''
+const Planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis = 'Analysis is discovering differences between what you want and what you have'
+const Design = 'Figure out how your layout is going to look like and implement your planning and analysis into the design concept'
+const Implementation = 'Apply the solution and the design. This is when the coding is done'
+const Testing = 'Testing making sure the solution works'
+const Integration = 'Shipping and deploying things to the user'
+const Maintain = 'Maintain what you did and repeat the process as needed'
